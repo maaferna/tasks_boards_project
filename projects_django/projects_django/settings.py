747685990718c4dc6020af191038b8df94c588b5
mparraf <maaferna@gmail.com>
2023-07-26 12:09:08 -0400

    Change database configurations, to hide environment variables

--- projects_django/projects_django/settings.py
+++ projects_django/projects_django/settings.py
@@ -12,15 +12,19 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 import os
+
+from dotenv import load_dotenv
+load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+#BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-hg-r_o(ghp2%+=ijw!c_#-x-%x9cqu47n_*+-p_#x6s+93f5$('
+SECRET_KEY = os.getenv('secret_key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -84,10 +88,10 @@ WSGI_APPLICATION = 'projects_django.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'mparraf_task_boards',
+        'NAME': os.getenv('db_name'),
         'USER': 'mparraf',
         'PASSWORD': 'claveadmin',
-        'HOST': 'postgresql-mparraf.alwaysdata.net',
+        'HOST': os.getenv('host_name'),
         'PORT': "5432"
     }
 }
